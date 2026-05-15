f62d09b6d32c0745c3c0d83f56cad75854426bb4
jeffmart33 <jeffmartins172@gmail.com>
2026-05-14 17:11:13 -0700

    Add rewrites and experimental config to Next.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   experimental: {
     serverComponentsExternalPackages: ['pg']
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/backend/:path*',
+        destination: 'https://winnie-egkb.onrender.com/:path*'
+      }
+    ];
   }
 };
 
